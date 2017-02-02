ebef423be331c5b2bc06b2822a3b922999a38d46
Roberto Restrepo <betorestrepo329@hotmail.com>
2017-02-02 12:43:55 -0500

    Estructura basica

---
+++ README.md
@@ -0,0 +1,4 @@
+contratosCcas
+=============
+
+A Symfony project created on January 17, 2017, 2:02 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        $router = $this->container->get('router');
+        $rol = $this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN');
+        if($rol){
+            return new RedirectResponse($router->generate('homepage2'), 307);
+        }else{
+            return new RedirectResponse($router->generate('supervisores'), 307);
+        }
+    }
+    
+    /**
+     * @Route("/home2", name="homepage2")
+     */
+    public function home2Action(Request $request)
+    {
+        
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        return $this->render('default/adminHome.html.twig',array('usuarioLogueado'=>$user));
+    }
+    
+    /**
+     * @Route("/supervisor", name="supervisores")
+     */
+    public function supervisorAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    
+}
